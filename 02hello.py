da3432e85a1a6a8920874758da941ff5c13653d0
黯然销魂 <304315265@qq.com>
2019-06-30 23:53:23 +0800

    加了格式化

--- 02hello.py
+++ 02hello.py
@@ -8,8 +8,23 @@ print("hello python")
 print("注释")
 
 
-
+##对不起连续输出三次
 print("对不起   " * 3)
 
-
+###两个*可以乘方计算
 print(2 ** 3)
+
+
+##格式化输出
+name ="Bruce"
+print("我的名字叫 %s" % name)
+
+age=20
+print("我今年 %d岁"  %age)
+
+height=1.78
+print("身高 %.2f米" % height)
+
+##在字符中如果要打%，就得用%%的方式
+life=0.25
+print("人生轨迹刚开始 %.2f %%" %(life*100))
